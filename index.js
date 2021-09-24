54caaa07a8297651ca4459b20ef449750a0780cb
MarcelinoGNeto <marcelino.gneto@gmail.com>
2021-09-23 21:20:49 -0300

    adicionando Handlebars

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const handlebars = require('express-handlebars')
+const Sequelize = require('sequelize');
+
+// Config
+    // Template Engine
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
+    // Conexão com o banco de dados MySql
+    const sequelize = new Sequelize('testenodejs', 'root', '1234567',{
+        host: 'localhost',
+        dialect: 'mysql'
+    })
+
+// Rotas
+    app.get('/cad', function(req, res){
+        res.render('formulario')
+    })
+
+app.listen(8081, function(){
+    console.log("Servidor rodando na url http://localhost:8081");
+});
